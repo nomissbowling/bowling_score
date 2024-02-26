045c96ba34d330ba4a0d7710b770240153852832
nomissbowling <nomissbowling@yahoo.co.jp>
2024-02-26 16:27:22 +0900

    version 0.1.0

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![doc(html_root_url = "https://docs.rs/bowling_score/0.1.0")]
+//! bowling_score bowling score app for Rust
+//!
+
+use std::error::Error;
+
+use bscore::bgame::bowling_score;
+
+/// main
+pub fn main() -> Result<(), Box<dyn Error>> {
+  bowling_score(false, "")
+}
+
+/// test with [-- --nocapture] or [-- --show-output]
+#[cfg(test)]
+mod tests {
+  use bscore::bgame::bscore;
+
+  /// test score
+  #[test]
+  fn test_score() {
+    assert_eq!(bscore("xxxxxxxxxxxxxx", true).unwrap(), [300, 300, 300]);
+  }
+}
